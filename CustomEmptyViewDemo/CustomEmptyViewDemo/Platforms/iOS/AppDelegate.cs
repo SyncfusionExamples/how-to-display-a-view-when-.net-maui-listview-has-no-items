f9361f0a1dfe65f417452b9ac0119c7022d5ae27
Jayashree N R <jayashree.ravishankar@syncfusion.com>
2022-11-24 17:42:18 +0530

    removed file

--- CustomEmptyViewDemo/CustomEmptyViewDemo/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CustomEmptyViewDemo;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
