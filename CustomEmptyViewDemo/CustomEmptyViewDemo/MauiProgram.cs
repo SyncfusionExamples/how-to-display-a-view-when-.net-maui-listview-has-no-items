7203ee1c9508cf60d18abfc41213aec48d3314b2
Jayashree N R <jayashree.ravishankar@syncfusion.com>
2022-11-26 11:27:30 +0530

    Alignment changes

--- CustomEmptyViewDemo/CustomEmptyViewDemo/MauiProgram.cs
+++ CustomEmptyViewDemo/CustomEmptyViewDemo/MauiProgram.cs
@@ -13,9 +13,9 @@ namespace CustomEmptyViewDemo
 				{
 					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("Roboto-Medium.ttf", "Roboto-Medium");
-                    fonts.AddFont("Roboto-Regular.ttf", "Roboto-Regular");
-                });
+					fonts.AddFont("Roboto-Medium.ttf", "Roboto-Medium");
+					fonts.AddFont("Roboto-Regular.ttf", "Roboto-Regular");
+				});
 			builder.ConfigureSyncfusionCore();
 
 			return builder.Build();
